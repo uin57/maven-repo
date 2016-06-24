9cc618d04c4d18f5e9406ce8b9467df8405b6f4f
feelgo <feelgo@foxmail.com>
2016-06-24 17:09:31 +0800

    update main.go

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"path"
-	"io/ioutil"
 	"path/filepath"
+	"strings"
 )
 
 var (
 	root = "/data"
-	mod = map[string]string{
-		"maven":"http://repo1.maven.org/maven2",
-		"gradle":"http://downloads.gradle.org/distributions",
+	mod  = map[string]string{
+		"maven":  "http://repo1.maven.org/maven2",
+		"gradle": "http://downloads.gradle.org/distributions",
 	}
 )
 
 func main() {
-	mux:=http.NewServeMux()
+	mux := http.NewServeMux()
 	mux.HandleFunc("/maven/", handler)
 	mux.HandleFunc("/gradle/", handler)
 	log.Println("Start serving on port 80")
@@ -39,45 +39,74 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		proxyMod = "maven"
 	} else if strings.HasPrefix(uri, "/gradle") {
 		proxyMod = "gradle"
-	}else{
+	} else {
 		w.WriteHeader(404)
 		return
 	}
-	realUri := strings.TrimPrefix(uri, "/" + proxyMod)
-	filename:=root+"/"+ proxyMod+ realUri
+	realUri := strings.TrimPrefix(uri, "/"+proxyMod)
+	filename := root + "/" + proxyMod + realUri
 	if path.Ext(realUri) != "" {
 		file, err := os.Open(filename)
+		defer file.Close()
 		if err == nil {
-			if bytes, e := ioutil.ReadAll(file); e == nil {
-				w.Write(bytes)
-				return
+			if _, err := io.Copy(w, file); err != nil {
+				w.WriteHeader(500)
 			}
+			return
 		}
 		isFile = true
 	}
-	if code,buf, respErr := doGet(mod[proxyMod] + realUri); respErr == nil {
-		if isFile && code==200 {
-			writeFile(buf, filename)
-		}
-		w.Write(buf)
-	} else {
+	log.Println(mod[proxyMod] + realUri)
+	if resp, err := http.Get(mod[proxyMod] + realUri); err != nil {
 		w.WriteHeader(500)
-		log.Panicln(respErr)
-		return
+		log.Println(err)
+	} else {
+		defer resp.Body.Close()
+		if resp.StatusCode == 200 {
+			if isFile {
+				os.MkdirAll(filepath.Dir(filename), os.ModePerm)
+				tempFile := filename + ".tmp"
+				if err := writeFile(tempFile, resp.Body); err != nil {
+					io.Copy(w, resp.Body)
+				} else {
+					os.Rename(tempFile, filename)
+					f, _ := os.Open(filename)
+					defer func() {
+						f.Close()
+					}()
+					if _, err := io.Copy(w, f); err != nil {
+						w.WriteHeader(500)
+					}
+				}
+			} else {
+				io.Copy(w, resp.Body)
+			}
+
+		} else {
+			w.WriteHeader(resp.StatusCode)
+		}
 	}
 }
-func doGet(url string) (code int,bytes []byte, err error) {
+
+func writeFile(file string, reader io.Reader) error {
+	if f, err := os.OpenFile(file, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666); err != nil {
+		return err
+	} else {
+		defer f.Close()
+		io.Copy(f, reader)
+		f.Sync()
+		return nil
+	}
+}
+
+func doGet(url string, writer io.Writer) (code int, bytes []byte, err error) {
 	log.Println(url)
 	resp, err := http.Get(url)
-	if err!=nil {
+	if err != nil {
 		log.Panic(err)
 	}
-	code=resp.StatusCode
+	code = resp.StatusCode
 	defer resp.Body.Close()
-	bytes ,err= ioutil.ReadAll(resp.Body)
+	_, err = io.Copy(writer, resp.Body)
 	return
-}
-func writeFile(bytes []byte, filename string) {
-	os.MkdirAll(filepath.Dir(filename), os.ModePerm)
-	ioutil.WriteFile(filename, bytes, 0666)
 }
